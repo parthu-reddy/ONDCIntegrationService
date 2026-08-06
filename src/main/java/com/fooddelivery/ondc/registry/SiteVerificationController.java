4504f059da208a40fe5fc139781e5ca2df7be797
Parthu <parthudhanireddy@gmail.com>
2026-08-06 20:24:06 +0530

    chore: sync all microservices changes

--- src/main/java/com/fooddelivery/ondc/registry/SiteVerificationController.java
+++ src/main/java/com/fooddelivery/ondc/registry/SiteVerificationController.java
@@ -5,12 +5,14 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Serves the ONDC site verification HTML file.
  */
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class SiteVerificationController {
 
     private final SiteVerificationService siteVerificationService;
